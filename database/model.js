2028497c9df0d1198ccef2a44b2d7eb711751352
Viny Selopal <viny0698@gmail.com>
2022-08-29 12:42:58 +0530

    removed unnecessary console.logs

--- database/model.js
+++ database/model.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 const todoSchema = new mongoose.Schema({
   content: String,
   done: Boolean,
-  priority: Number,
+  priority: String,
   notes: String,
   date: String
 })
--- server.js
+++ server.js
@@ -20,9 +20,7 @@ app.use(
 
 app.post('/', async (req, res) => {
   try {
-    console.log('post req')
     const result = await insertTodo(req.body)
-    console.log('result', result)
     res.json(result)
   } catch (err) {
     console.log(err)
@@ -30,9 +28,7 @@ app.post('/', async (req, res) => {
 })
 app.get('/', async (req, res) => {
   try {
-    console.log('get all req')
     const result = await getAllTodos()
-    console.log('result', result)
     res.json(result)
   } catch (err) {
     console.log(err)
@@ -41,9 +37,7 @@ app.get('/', async (req, res) => {
 app.put('/:id', async (req, res) => {
   try {
     const {id} = req.params
-    console.log('update req')
     const result = await updateTodo({'id': ObjectId(id)}, req.body)
-    console.log('result', result)
     res.json(result)
   } catch (err) {
     console.log(err)
@@ -51,20 +45,7 @@ app.put('/:id', async (req, res) => {
 })
 app.delete('/', async (req, res) => {
   try {
-    console.log('deleteAll req')
     const result = await deleteAll()
-    console.log('result', result)
-    res.json(result)
-  } catch (err) {
-    console.log(err)
-  }
-})
-app.delete('/:id', async (req, res) => {
-  try {
-    const { id } = req.params
-    console.log('delete req')
-    const result = await deleteTodo({'id': ObjectId(id)})
-    console.log('result', result)
     res.json(result)
   } catch (err) {
     console.log(err)
@@ -72,14 +53,23 @@ app.delete('/:id', async (req, res) => {
 })
 app.delete('/done', async (req, res) => {
   try {
-    console.log('delete req')
     const result = await deleteDone(req.body)
-    console.log('result', result)
     res.json(result)
   } catch (err) {
     console.log(err)
   }
 })
+
+app.delete('/:id', async (req, res) => {
+  try {
+    const { id } = req.params
+    const result = await deleteTodo({'id': ObjectId(id)})
+    res.json(result)
+  } catch (err) {
+    console.log(err)
+  }
+})
+
 
 
 app.listen(port, () => {
